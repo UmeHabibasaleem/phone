e187915faf0101b290ef4119bd7e86d26c4cc7bd
UmeHabibasaleem <38471790+UmeHabibasaleem@users.noreply.github.com>
2018-10-16 20:22:25 +0500

    my second commit
    this is my second commit

---
+++ WebApplication1/WebApplication1/Models/UpdateBirthdayViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class UpdateBirthdayViewModel
+    {
+        public List<PersonViewModel> UpdatePerson = new List<PersonViewModel>();
+        public List<PersonViewModel> Person = new List<PersonViewModel>();
+    }
+}
